c9637b368a298bfe82592391bb75d536734e2c14
Richard Helgeby <Richard Helgeby@localhost>
2010-07-12 03:20:28 +0200

    Updated compilers with hash table update (1.4.0-dev hg3078).

--- compile.bat
+++ compile.bat
@@ -3,7 +3,7 @@
 set SOURCEDIR=src
 set SMINCLUDES=env\include
 set BUILDDIR=build
-set SPCOMP=env\win32\bin\spcomp-1.4.0-3074.exe
+set SPCOMP=env\win32\bin\spcomp-1.4.0-3078.exe
 set VERSIONDUMP=updateversion.bat
 
 :: Dump version and revision information first.
